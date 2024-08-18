6ecbee2f0318454cd5824f678d387d5cdf4674f5
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2024-08-18 21:00:38 +0600

    latest

--- routes/users/index.js
+++ routes/users/index.js
@@ -10,18 +10,8 @@ router.patch("/forgot-password", user.forgotPassword);
 router.post("/refresh-token", user.refreshToken);
 router.get("/", user.getAllUsers);
 router.delete("/:id", user.deleteUser);
-router.get("/:id", auth("admin"), user.getSingleUser);
 
 
-// router.post("/signup", user.signup);
-// router.post("/login", user.login);
-// router.get("/", user.getAllUsers);
-// router.delete("/", user.deleteUser);
-// router.patch("/", user.updateUser);
 
-
-// router.get("/", verifyToken, authorization("super_admin"), user.getAllUsers);
-router.get("/:id", user.getSingleUser);
-// router.get("/", verifyToken, user.getAllUsers);
 
 module.exports = router;
